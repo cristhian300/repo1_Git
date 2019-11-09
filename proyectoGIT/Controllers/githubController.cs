e93eb4977409677f4d458367528fd1999e76835d
Cristhian <cristhian_666win@hotmail.com>
2019-11-09 10:39:12 -0500

    cntroller git

---
+++ proyectoGIT/Controllers/githubController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace proyectoGIT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class githubController : ControllerBase
+    {
+    }
+}
